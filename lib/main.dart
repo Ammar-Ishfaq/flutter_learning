85f36b3d821baca4dc7302e811a22ebf2dea8454
ammar <ammarishfaq25@gmail.com>
2022-11-09 17:27:13 +0500

    - snack bar doneˆ

--- lib/main.dart
+++ lib/main.dart
@@ -23,7 +23,8 @@ void main() {
   // runApp(mHorizontalList());
   // runApp(GridListWithToast());
   // runApp(CheckBoxAndRadioButton());
-  runApp(LinearProgressBar());
+  // runApp(LinearProgressBar());
+  runApp(SnackBarDemo());
 }
 
 class MyApp extends StatelessWidget {
@@ -1320,5 +1321,47 @@ class CircularProgressBar_ extends StatelessWidget {
     );
   }
 }
+class SnackBarDemo extends StatelessWidget {
+  // const SnackBarDemo({super.key});
 
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'SnackBar Demo',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('SnackBar Demo'),
+        ),
+        body:  SnackBarPage(),
+      ),
+    );
+  }
+}
 
+class SnackBarPage extends StatelessWidget {
+  // const SnackBarPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: ElevatedButton(
+        onPressed: () {
+          final snackBar = SnackBar(
+            content: const Text('Yay! A SnackBar!'),
+            action: SnackBarAction(
+              label: 'Undo',
+              onPressed: () {
+                // Some code to undo the change.
+              },
+            ),
+          );
+
+          // Find the ScaffoldMessenger in the widget tree
+          // and use it to show a SnackBar.
+          ScaffoldMessenger.of(context).showSnackBar(snackBar);
+        },
+        child: const Text('Show SnackBar'),
+      ),
+    );
+  }
+}
